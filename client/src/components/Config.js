b00920aa14dd19362f07be3b475de814d758f485
whrrk <jwh8712@naver.com>
2021-01-18 18:37:59 +0900

    movie start

---
+++ client/src/components/Config.js
@@ -0,0 +1,7 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = '48164d2a3077f43bb44f63997ea29ce4';
+
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/'
---
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import { FaCode } from "react-icons/fa";
+import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config';
+import MainImage from './Sections/MainImage';
+
+function LandingPage() {
+
+
+    const [Movies, setMovies] = useState([])
+    const [MainMovieImage, setMainMovieImage] = useState()
+
+    useEffect(() => {
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+        fetch(endpoint)
+            .then(response => response.json())
+            .then(response => {
+                setMovies([response.results])
+                setMainMovieImage(response.results[2])
+            })
+    }, [])
+    return (
+        <div style={{ width: '100%', margin: '0' }}>
+            {MainMovieImage &&
+                <MainImage image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
+                    title={MainMovieImage.original_title}
+                    text={MainMovieImage.overview}
+                />
+            }
+
+            <div style={{ width: '85%', margin: '1rem auto' }}>
+                <h2> Movies by latest </h2>
+                <hr />
+
+            </div>
+
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <button> Load More </button>
+            </div>
+        </div>
+
+
+    )
+}
+
+export default LandingPage
