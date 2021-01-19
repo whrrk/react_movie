4f89ea800077773de0864dc68c452dcf4c960bd5
whrrk <jwh8712@naver.com>
2021-01-19 18:25:48 +0900

    day2 -gridcards

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { FaCode } from "react-icons/fa";
 import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config';
 import MainImage from './Sections/MainImage';
+import GridCards from '../commons/GridCards';
+import { Row } from 'antd';
 
 function LandingPage() {
 
-
-    const [Movies, setMovies] = useState([])
+    const [Movies, setMovies] = useState()
     const [MainMovieImage, setMainMovieImage] = useState()
 
     useEffect(() => {
@@ -14,8 +15,10 @@ function LandingPage() {
         fetch(endpoint)
             .then(response => response.json())
             .then(response => {
-                setMovies([response.results])
+
+                setMovies(response.results)
                 setMainMovieImage(response.results[1])
+
             })
     }, [])
     return (
@@ -31,12 +34,25 @@ function LandingPage() {
                 <h2> Movies by latest </h2>
                 <hr />
 
+                <Row gutter={[16, 16]}>
+                    {Movies && Movies.map((movie, index) => (
+
+                        <React.Fragment key={index}>
+                            <GridCards image={movie.poster_path ? `${IMAGE_BASE_URL}w500${movie.poster_path}` : null}
+                                movieId={movie.id}
+                                movieName={movie.original_title}
+                            />
+                        </React.Fragment>
+                    ))}
+                </Row>
+
+
             </div>
 
             <div style={{ display: 'flex', justifyContent: 'center' }}>
                 <button> Load More </button>
             </div>
-        </div>
+        </div >
 
 
     )
