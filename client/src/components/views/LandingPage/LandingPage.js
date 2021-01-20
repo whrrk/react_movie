7306945c02ef8d1e0b220623352aef88b4d0655a
whrrk <jwh8712@naver.com>
2021-01-20 15:44:42 +0900

    front - load more button - movie detail page

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -7,20 +7,31 @@ import { Row } from 'antd';
 
 function LandingPage() {
 
-    const [Movies, setMovies] = useState()
-    const [MainMovieImage, setMainMovieImage] = useState()
+    const [Movies, setMovies] = useState([])
+    const [MainMovieImage, setMainMovieImage] = useState(null)
+    const [CurrentPage, setCurrentPage] = useState(0)
 
     useEffect(() => {
         const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+        fetchMovies(endpoint)
+    }, [])
+
+
+    const fetchMovies = (endpoint) => {
         fetch(endpoint)
             .then(response => response.json())
             .then(response => {
-
-                setMovies(response.results)
-                setMainMovieImage(response.results[1])
-
+                setMovies([...Movies, ...response.results])
+                setMainMovieImage(response.results[0])
+                setCurrentPage(response.page)
             })
-    }, [])
+    }
+
+    const loadMoreItems = () => {
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${CurrentPage + 1}`
+        fetchMovies(endpoint)
+    }
+
     return (
         <div style={{ width: '100%', margin: '0' }}>
             {MainMovieImage &&
@@ -50,7 +61,7 @@ function LandingPage() {
             </div>
 
             <div style={{ display: 'flex', justifyContent: 'center' }}>
-                <button> Load More </button>
+                <button onClick={loadMoreItems}> Load More </button>
             </div>
         </div >
 
