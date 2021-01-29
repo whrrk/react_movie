683fed287d29b0ba1de13f48266c265348b12138
whrrk <jwh8712@naver.com>
2021-01-29 15:43:37 +0900

    front - 좋아하는영화 추가

--- client/src/components/views/MovieDetail/Section/Favorite.js
+++ client/src/components/views/MovieDetail/Section/Favorite.js
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Axios from 'axios'
+import { Button } from 'antd'
 
 function Favorite(props) {
 
@@ -9,30 +10,78 @@ function Favorite(props) {
     const moviePost = props.movieInfo.backdrop_path
     const movieRunTime = props.movieInfo.runtime
 
+    const [FavoriteNumber, setFavoriteNumber] = useState(0)
+    const [Favorited, setFavorited] = useState(false)
+
+
+    let variables = {
+        userFrom: userFrom,
+        movieId: movieId,
+        moviePost: moviePost,
+        movieTitle: movieTitle,
+        movieRunTime: movieRunTime
+    }
+
     useEffect(() => {
         //이 영화를 얼마나 많은 사람이 favorite 했는지 정보를 얻을 수 있는 로직
-
-        let variables = {
-            userFrom,
-            movieId
-        }
 
         //fetch()를 쓸수도있고 Axios로 할 수도 있음.
         Axios.post('/api/favorite/favoriteNumber', variables)
             .then(response => {
                 //response에 결과값이 들어옴
+                setFavoriteNumber(response.data.favoriteNumber)
+
                 if (response.data.success) {
                     //favoriteNumber: 0 success: true 이런식으로 나옴
+
                 } else {
                     alert('숫자 정보를 가져오는데 실패 함.')
                 }
             })
 
+        Axios.post('/api/favorite/favorited', variables)
+            .then(response => {
+                //response에 결과값이 들어옴
+
+                if (response.data.success) {
+                    setFavorited(response.data.favorited)
+                } else {
+                    alert('정보를 가져오는데 실패 함.')
+                }
+            })
+
     }, [])
+
+
+    const onClickFavorite = () => {
+
+        if (Favorited) {
+            Axios.post('/api/favorite/removeFromFavorite', variables)
+                .then(response => {
+                    if (response.data.success) {
+                        setFavoriteNumber(FavoriteNumber - 1)
+                        setFavorited(!Favorited)
+                    } else {
+                        alert('Favorite list remove failed')
+                    }
+                })
+        }
+        else {
+            Axios.post('/api/favorite/addToFavorite', variables)
+                .then(response => {
+                    if (response.data.success) {
+                        setFavoriteNumber(FavoriteNumber + 1)
+                        setFavorited(!Favorited)
+                    } else {
+                        alert('Favorite list add failed')
+                    }
+                })
+        }
+    }
 
     return (
         <div>
-            <button>Favorite</button>
+            <Button onClick={onClickFavorite}>{Favorited ? " Not Favorite" : "Add to Favorited"} {FavoriteNumber} </Button>
         </div>
     )
 }
--- server/routes/favorite.js
+++ server/routes/favorite.js
@@ -17,4 +17,44 @@ router.post('/favoriteNumber', (req, res) => {
 })
 
 
+router.post('/favorited', (req, res) => {
+
+    //내가 이 영화를 좋아하는 리스트에 추가 했는지 안했는지 DB에서 가져오기
+    //mongoDB에서 favorite 숫자를 가져오게함
+    Favorite.find({ "movieInd": req.body.movieId, "userFrom": req.body.userFrom })
+        .exec((err, info) => {
+            if (err) return res.status(400).send(err)
+
+            let result = false;
+            if (info.length !== 0) {
+                result = true
+            }
+
+            res.status(200).json({ success: true, favorited: result })
+        })
+    //그 다음 프론트에 다시 숫자 정보를 보내줌
+
+})
+
+router.post('/addToFavorite', (req, res) => {
+
+    const favorite = new Favorite(req.body)
+
+    favorite.save((err, doc) => {
+        if (err) return res.status(400).send(err)
+        return res.status(200).json({ success: true })
+    })
+
+})
+
+router.post('/removeFromFavorite', (req, res) => {
+
+    Favorite.findOneAndDelete({ movieId: req.body.movieId, userFrom: req.body.userFrom })
+        .exec((err, doc) => {
+            if (err) return res.status(400).send(err)
+            return res.status(200).json({ success: true, doc })
+        })
+})
+
+
 module.exports = router;
